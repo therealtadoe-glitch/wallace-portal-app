6342c0404f34be088f36168cc1858e50332393e4
tadoe_cant_code <146056198+therealtadoe-glitch@users.noreply.github.com>
2026-03-29 07:23:27 -0500

    Revert "Mobile-first Godot 4.6 landscaping ops app with local-first data layer"

--- new-game-project/scripts/utils/id_generator.gd
+++
@@ -1,8 +0,0 @@
-extends RefCounted
-class_name AppIdGenerator
-
-static var _counter: int = 0
-
-static func next(prefix: String) -> String:
-	_counter += 1
-	return "%s_%s_%d" % [prefix, Time.get_datetime_string_from_system().replace(":", "").replace("-", "").replace("T", ""), _counter]
